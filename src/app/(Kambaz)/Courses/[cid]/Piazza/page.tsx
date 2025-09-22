c7c2702a1bd56797feb5f911833b38f7b618dfd2
Ella <pitts.ell@northeastern.edu>
2025-09-22 16:20:43 -0400

    Fixed the Piazza page to go to real Piazza (for now)

--- src/app/(Kambaz)/Courses/[cid]/Piazza/page.tsx
+++ src/app/(Kambaz)/Courses/[cid]/Piazza/page.tsx
@@ -1,10 +1,16 @@
+import Link from "next/link";
+
 export default function Piazza() {
   return (
     <div id="wd-piazza">
       <h1>Piazza Page</h1>
       <p>
-        Welcome to the Piazza page. This is a placeholder for future content.
+        Welcome! Click the button the go to the real NEU Piazza page (while we
+        wait to work on our own version).
       </p>
+      <Link href="https://piazza.com/northeastern" id="wd-course-piazza-link">
+        <button className="btn btn-primary">Piazza </button>
+      </Link>
     </div>
   );
 }
