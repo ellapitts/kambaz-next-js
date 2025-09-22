b1f44800d116ac7c5aedc45481b32eb6a34503df
Ella <pitts.ell@northeastern.edu>
2025-09-22 19:52:37 -0400

    changed the naem of navigation to MyNavigation

---
+++ src/app/(Kambaz)/Account/MyNavigation.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+export default function AccountNavigation() {
+  return (
+    <div id="wd-account-navigation">
+      <Link href="Signin"></Link> <br />
+      <Link href="Signup"></Link> <br />
+      <Link href="Profile">Profile</Link> <br />
+    </div>
+  );
+}
--- src/app/(Kambaz)/Account/Navigation.tsx
+++
@@ -1,9 +0,0 @@
-import Link from "next/link";
-export default function AccountNavigation() {
- return (
-   <div id="wd-account-navigation">
-     <Link href="Signin"></Link> <br />
-     <Link href="Signup"></Link> <br />
-     <Link href="Profile">Profile</Link> <br />
-   </div>
-);} 
