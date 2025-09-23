d7d8a3a60ff1603b1b114d202f9a6658ac685189
Ella <pitts.ell@northeastern.edu>
2025-09-22 23:13:51 -0400

    fixing home page buttons

--- src/app/(Kambaz)/Courses/[cid]/Home/status.tsx
+++ src/app/(Kambaz)/Courses/[cid]/Home/status.tsx
@@ -2,6 +2,15 @@ export default function CourseStatus() {
   return (
     <div id="wd-course-status">
       <h2>Course Status</h2>
+      <button>Collapse All</button>
+      <button>View Progress</button>
+      <select id="wd-publish-all">
+        <option value="">✓ Publish All</option>
+        <option value="publish-modules">Publish All Modules and Items</option>
+        <option value="publish-modules-only">Publish Modules Only</option>
+        <option value="unpublish-all">Unpublish All</option>
+      </select>
+
       <button>Unpublish</button>
       <button>Publish</button>
       <button>Import Existing Content</button>
