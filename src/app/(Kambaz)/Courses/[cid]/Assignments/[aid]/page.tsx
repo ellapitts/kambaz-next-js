101693e03d7feda93ab026aaec63c4879335ca20
Ella <pitts.ell@northeastern.edu>
2025-09-22 23:40:51 -0400

    aid

--- src/app/(Kambaz)/Courses/[cid]/Assignments/[aid]/page.tsx
+++ src/app/(Kambaz)/Courses/[cid]/Assignments/[aid]/page.tsx
@@ -2,29 +2,39 @@
 // Content: Assignment name field, description, points field, due date field, submission type (link/file) field etc.
 // This is mostly for the Faculty/Professor View. Student does not have this accessibility feature in the future.
 
-export default function AssignmentEditor() {
+export default async function AssignmentEditor({
+  params,
+}: {
+  params: Promise<{ cid: string; aid: string }>;
+}) {
+  const { cid, aid } = await params;
+
   return (
     <div id="wd-assignments-editor">
-      <h2>Assignments</h2>
-      <label htmlFor="wd-name">Assignment Name</label>
-      <input id="wd-name" defaultValue="A1 - ENV + HTML" />
+      <h1>Assignments</h1>
+      <input id="wd-name" defaultValue="A1" />
       <br />
       <br />
+
       <textarea
         id="wd-description"
-        defaultValue="The assignment is available online Submit a link to the landing page"
+        rows={4}
+        cols={50}
+        defaultValue="The assignment is available online. Submit a link to the landing page"
       />
       <br />
+      <br />
+      <div style={{ marginBottom: "10px" }}>
+        <label htmlFor="wd-due-date">Due Date: </label>
+        <input id="wd-due-date" type="date" defaultValue="2023-10-10" />
+      </div>
       <table>
-        <tr>
-          <td align="right" valign="top">
-            <label htmlFor="wd-points">Points</label>
-          </td>
-          <td>
-            <input id="wd-points" defaultValue={100} />
-          </td>
-        </tr>
-        {/* Complete on your own */}
+        <div>
+          <label htmlFor="wd-points" style={{ marginRight: "10px" }}>
+            <strong>Points</strong>
+          </label>
+          <input id="wd-points" type="Letter" defaultValue={100} />
+        </div>
       </table>
     </div>
   );
