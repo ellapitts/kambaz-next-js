d5449ac5dd17e56628734611d83709ba332dafd0
Ella <pitts.ell@northeastern.edu>
2025-09-29 19:58:32 -0400

    Makign index css and tsx pages

---
+++ src/app/(Kambaz)/Courses/[cid]/Home/index.tsx
@@ -0,0 +1,13 @@
+import CourseNavigation from "./[cid]/navigation";
+import Modules from "./Modules";
+export default function Home() {
+    return (
+        <table id="wd-home">
+            <tr>
+                <td valign="top">
+                    <Modules
+                </td>
+            </tr>
+            <
+        </table>
+    )
