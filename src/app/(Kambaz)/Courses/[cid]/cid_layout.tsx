cd66d83e8e9d5dbe2b0c2d8a4c1856d2e31dcd1f
Ella <pitts.ell@northeastern.edu>
2025-09-30 14:37:59 -0400

    fixing layout

--- src/app/(Kambaz)/Courses/[cid]/cid_layout.tsx
+++ src/app/(Kambaz)/Courses/[cid]/cid_layout.tsx
@@ -1,13 +1,8 @@
 // Presereve the Kambaz navigatuion and layout for all courses pages.
 import { ReactNode } from "react";
-import CourseNavigation from "./MyNavigation";
-
-export default async function CoursesLayout({
-  children,
-  params,
-}: Readonly<{ children: ReactNode; params: Promise<{ cid: string }> }>) {
+import CourseNavigation from "./cid_navigation";
+export default async function CoursesLayout({children, params}: Readonly<{ children: ReactNode; params: Promise<{ cid: string }> }>) {
   const { cid } = await params;
-
   return (
     <div id="wd-courses">
       <h2>Courses {cid}</h2>
@@ -20,18 +15,14 @@ export default async function CoursesLayout({
           <option value="publish-modules">Publish All Modules</option>
           <option value="publish-items">Publish All Items</option>
           <option value="unpublish-all">Unpublish</option>
-        </select>{" "}
+        </select>
         <button id="wd-add-module">+ Module Changes</button>
       </div>
-
       <table>
         <tbody>
           <tr>
-            <td valign="top" width="200">
-              <CourseNavigation />
-            </td>
-            <td valign="top" width="100%">
-              {children}{" "}
+            <td valign="top" width="200"><CourseNavigation /></td>
+            <td valign="top" width="100%">{children}
             </td>
           </tr>
         </tbody>
