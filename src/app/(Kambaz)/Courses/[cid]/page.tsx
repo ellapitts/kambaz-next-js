052052317d99be881f8d40086795fde52acaf6f9
Ella <pitts.ell@northeastern.edu>
2025-09-30 15:17:59 -0400

    Lumping all code into one block

--- src/app/(Kambaz)/Courses/[cid]/page.tsx
+++ src/app/(Kambaz)/Courses/[cid]/page.tsx
@@ -2,15 +2,14 @@
 
 // The redirect navigation for the courses to go home page.
 // This is also the placeholder until the course pages are created later.
-
-import { redirect } from "next/navigation";
-
-export default async function CoursesPage({ params,}: {params: Promise<{ cid: string }>;}) {
-  const { cid } = await params;
-  redirect('/Courses/${cid}/Home');
-;}
   
   // return (
   //   <div id="wd-courses">
   //     <h1>Courses 1234</h1>
   //     </div>
+// src/(Kambaz)/Courses/[cid]/page.tsx
+import { redirect } from "next/navigation";
+export default async function CoursesPage({ params,}: {params: Promise<{ cid: string }>;}) {
+  const { cid } = await params;
+  redirect(`/Courses/${cid}/Home`);
+}
