fc1f1775dbfbf1710926c801309847854fcd998f
Ella <pitts.ell@northeastern.edu>
2025-10-05 13:53:52 -0400

    Fixed single quote

--- src/app/Labs/Lab2/page.tsx
+++ src/app/Labs/Lab2/page.tsx
@@ -7,7 +7,7 @@ export default function Lab2() {
       <h3>Styling with the STYLE attribute</h3>
       <p style={{ backgroundColor: "blue", color: "white" }}>
         Style attribute allows configuring look and feel right on the element.
-        Although it's very convenient it is considered bad practice and you
+        Although its very convenient it is considered bad practice and you
         should avoid using the style attribute
       </p>
     </div>
