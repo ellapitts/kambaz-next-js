4fa2ec04c9edd62fb03757871bf1ad6b7b625f26
Ella <pitts.ell@northeastern.edu>
2025-10-06 14:47:28 -0400

    Changing from table to CSS Bootstrap styling

--- src/app/(Kambaz)/layout.tsx
+++ src/app/(Kambaz)/layout.tsx
@@ -8,15 +8,28 @@ import KambazNavigation from "./kambaz-homepage-navigation";
 
 export default function KambazLayout({children,}: Readonly<{ children: ReactNode }>) {
   return (
+    // Old code with the table
+    // <div id="wd-kambaz">
+    //   <table>
+    //   <tbody>
+    //       <tr>
+    //         <td valign="top" width="200"><KambazNavigation /></td>
+    //         <td valign="top" width="100%">{children}</td>
+    //       </tr>
+    //     </tbody>
+    //   </table>
+    // </div>
+
+    // New code 2.4 Styling Kambaz with CSS and Bootstrap
     <div id="wd-kambaz">
-      <table>
-      <tbody>
-          <tr>
-            <td valign="top" width="200"><KambazNavigation /></td>
-            <td valign="top" width="100%">{children}</td>
-          </tr>
-        </tbody>
-      </table>
+      <div className="d-flex">
+        <div>
+          <KambazNavigation />
+        </div>
+        <div className="flex-fill">
+          {children}
+        </div>
+      </div>
     </div>
   );
 }
