52c713ccdd746d12feb299f7697873a77f159685
Ella <pitts.ell@northeastern.edu>
2025-10-06 14:44:17 -0400

    Fixed the image path for it to render

--- src/app/Labs/Lab2/BootstrapNavigation.tsx
+++ src/app/Labs/Lab2/BootstrapNavigation.tsx
@@ -1,4 +1,14 @@
-import { Nav, NavItem, NavLink, Card, CardImg, CardTitle, CardBody, CardText, Button } from "react-bootstrap";
+import {
+  Nav,
+  NavItem,
+  NavLink,
+  Card,
+  CardImg,
+  CardTitle,
+  CardBody,
+  CardText,
+  Button,
+} from "react-bootstrap";
 
 export default function BootstrapNavigation() {
   return (
@@ -23,19 +33,18 @@ export default function BootstrapNavigation() {
 
       {/*  Bootstrap Cards */}
       <div id="wd-css-navigating-with-cards">
-  <h2> Cards </h2>
-  <Card style={{ width: "18rem" }}>
-    <CardImg variant="top" src="images/stacked.jpg" />
-    <CardBody>
-      <CardTitle>Stacking Starship</CardTitle>
-      <CardText>
-        Stacking the most powerful rocket in history. Mars or bust!
-      </CardText>
-      <Button variant="primary">Boldly Go</Button>
-    </CardBody>
-  </Card>
-</div>
-
+        <h2> Cards </h2>
+        <Card style={{ width: "18rem" }}>
+          <CardImg variant="top" src="/images/stacked.jpg" />
+          <CardBody>
+            <CardTitle>Stacking Starship</CardTitle>
+            <CardText>
+              Stacking the most powerful rocket in history. Mars or bust!
+            </CardText>
+            <Button variant="primary">Boldly Go</Button>
+          </CardBody>
+        </Card>
+      </div>
     </div>
   );
 }
