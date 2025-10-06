b28ddc3107e2bf32be1df7ac65bd77ef4aea0224
Ella <pitts.ell@northeastern.edu>
2025-10-06 16:00:27 -0400

    Styling people page

--- src/app/(Kambaz)/Courses/[cid]/People/Table/page.tsx
+++ src/app/(Kambaz)/Courses/[cid]/People/Table/page.tsx
@@ -1,8 +1,103 @@
+import { Table } from "react-bootstrap";
+import { FaUserCircle } from "react-icons/fa";
 export default function People() {
   return (
-    <div id="wd-people">
-      <h1>People</h1>
-      <p>Find your classmates soon...integrated later</p>
+    <div id="wd-people-table">
+      <Table striped>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Login ID</th>
+            <th>Section</th>
+            <th>Role</th>
+            <th>Last Activity</th>
+            <th>Total Activity</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* Person 1 - Tony Stark */}
+          <tr>
+            <td className="wd-full-name text-nowrap">
+              <FaUserCircle className="me-2 fs-1 text-secondary" />
+              <span className="wd-first-name">Tony</span>{" "}
+              <span className="wd-last-name">Stark</span>
+            </td>
+            <td className="wd-login-id">001234561S</td>
+            <td className="wd-section">S101</td>
+            <td className="wd-role">STUDENT</td>
+            <td className="wd-last-activity">2020-10-01</td>
+            <td className="wd-total-activity">10:21:32</td>
+          </tr>
+
+          {/* Person 2 - Bruce Wayne */}
+          <tr>
+            <td className="wd-full-name text-nowrap">
+              <FaUserCircle className="me-2 fs-1 text-secondary" />
+              <span className="wd-first-name">Bruce</span>{" "}
+              <span className="wd-last-name">Wayne</span>
+            </td>
+            <td className="wd-login-id">001234562S</td>
+            <td className="wd-section">S101</td>
+            <td className="wd-role">STUDENT</td>
+            <td className="wd-last-activity">2020-10-06</td>
+            <td className="wd-total-activity">10:00:00</td>
+          </tr>
+          {/* Person 3 - Steve Rogers */}
+          <tr>
+            <td className="wd-full-name text-nowrap">
+              <FaUserCircle className="me-2 fs-1 text-secondary" />
+              <span className="wd-first-name">Steve</span>{" "}
+              <span className="wd-last-name">Rogers</span>
+            </td>
+            <td className="wd-login-id">001234563S</td>
+            <td className="wd-section">S101</td>
+            <td className="wd-role">STUDENT</td>
+            <td className="wd-last-activity">2020-10-06</td>
+            <td className="wd-total-activity">10:00:00</td>
+          </tr>
+          {/* Person 4 - Natasha Romanoff  */}
+          <tr>
+            <td className="wd-full-name text-nowrap">
+              <FaUserCircle className="me-2 fs-1 text-secondary" />
+              <span className="wd-first-name">Natasha</span>{" "}
+              <span className="wd-last-name">Romanoff</span>
+            </td>
+            <td className="wd-login-id">001234574S</td>
+            <td className="wd-section">S101</td>
+            <td className="wd-role">STUDENT</td>
+            <td className="wd-last-activity">2020-10-06</td>
+            <td className="wd-total-activity">10:00:00</td>
+          </tr>
+
+          {/* Person 5 - Thor Odinson  */}
+          <tr>
+            <td className="wd-full-name text-nowrap">
+              <FaUserCircle className="me-2 fs-1 text-secondary" />
+              <span className="wd-first-name">Thor</span>{" "}
+              <span className="wd-last-name">Odinson</span>
+            </td>
+            <td className="wd-login-id">001234575S</td>
+            <td className="wd-section">S101</td>
+            <td className="wd-role">STUDENT</td>
+            <td className="wd-last-activity">2020-10-06</td>
+            <td className="wd-total-activity">10:06:00</td>
+          </tr>
+          
+          {/* Person 6 - Ella Pitts  */}
+          <tr>
+            <td className="wd-full-name text-nowrap">
+              <FaUserCircle className="me-2 fs-1 text-secondary" />
+              <span className="wd-first-name">Ella</span>{" "}
+              <span className="wd-last-name">Pitts</span>
+            </td>
+            <td className="wd-login-id">001234576S</td>
+            <td className="wd-section">S101</td>
+            <td className="wd-role">STUDENT</td>
+            <td className="wd-last-activity">2020-10-06</td>
+            <td className="wd-total-activity">12:00:00</td>
+          </tr>
+        </tbody>
+      </Table>
     </div>
   );
 }
