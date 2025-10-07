6e3ae5dfc246fe7554f20eea4ddf7257a18fdf09
Ella <pitts.ell@northeastern.edu>
2025-10-06 20:53:23 -0400

    Fixed href paths with sigh in, sign up, and profile

--- src/app/(Kambaz)/Account/Signin/page.tsx
+++ src/app/(Kambaz)/Account/Signin/page.tsx
@@ -12,7 +12,7 @@ export default function Signin() {
         type="password"
         className="mb-2"/>
       <Link id="wd-signin-btn" 
-      href="/Account/Profile"
+      href="/Dashboard"
       className="btn btn-primary w-100 mb-2">
         Sign in </Link>
       <Link id="wd-signup-link" href="/Account/Signup">Sign up </Link>
