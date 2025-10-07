a259f1eb4d4605bef14522620782195b3bd27e73
Ella <pitts.ell@northeastern.edu>
2025-10-06 22:31:27 -0400

    Merge branch 'main' of https://github.com/ellapitts/kambaz-next-js

--- src/app/Labs/Lab2/page.tsx
+++ src/app/Labs/Lab2/page.tsx
@@ -22,9 +22,6 @@ export default function Lab2() {
   return (
     <Container>
       <h2>Lab 2 - Cascading Style Sheets</h2>
-      <h3>Styling with the STYLE attriubte</h3>
-    {/* <div id="wd-lab2"> */}
-      <h2>Lab 2 - Cascading Style Sheets</h2>
       <h3>Styling with the STYLE attribute</h3>
       <div id="wd-css-id-selectors">
         <h3>ID selectors</h3>
