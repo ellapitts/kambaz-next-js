923df131c39871ae27b3b355d06e4698c99ef214
Ella <pitts.ell@northeastern.edu>
2025-10-19 20:40:11 -0400

    making the indiv courses dynamic with the data

--- src/app/(Kambaz)/Courses/[cid]/layout.tsx
+++ src/app/(Kambaz)/Courses/[cid]/layout.tsx
@@ -1,55 +1,22 @@
 /* src/app/(Kambaz)/Courses/[cid]/layout file is responsible for the course-specific navigation sidebar.
 - should have Home, Modules, Piazza, Zoom, Assignments, Quizzes, Grades, People. 
 */
-// Presereve the Kambaz navigatuion and layout for all courses pages.
-import { ReactNode } from "react";
-import CourseNavigation from "./cid_navigation";
-import { FaAlignJustify } from "react-icons/fa";
-
-export default async function CoursesLayout({
-  children,
-  params,
-}: Readonly<{ children: ReactNode; params: Promise<{ cid: string }> }>) {
-  const { cid } = await params;
-
+// Presereve the Kambaz navigation and layout for all courses pages.
+import { ReactNode } from "react"; // needed for children
+import CourseNavigation from "./cid_navigation"; // needed for the course nav. sidebar
+import { FaAlignJustify } from "react-icons/fa"; // needed for the heading
+import { courses } from "../../Database"; // used to find the current course
+ 
+export default function CoursesLayout({ children, params }: { children: ReactNode; params: { cid: string } | Promise<{ cid: string}>;}) {
+  const { cid } = params; 
+  const course = courses.find((course) => course._id === cid);
+  
   return (
-    // Old Styling
-    // <div id="wd-courses">
-    //   <h2>Courses {cid}</h2>
-    //   <hr />
-
-    //   <div id="wd-course-controls" style={{ marginBottom: "20px" }}>
-    //     <button id="wd-collapse-all">Collapse All</button>
-    //     <button id="wd-view-progress">View Progress</button>
-    //     <select id="wd-publish-all">
-    //       <option>Publish All</option>
-    //       <option value="publish-modules">Publish All Modules</option>
-    //       <option value="publish-items">Publish All Items</option>
-    //       <option value="unpublish-all">Unpublish</option>
-    //     </select>{" "}
-    //     <button id="wd-add-module">+ Module Changes</button>
-    //   </div>
-
-    //   <table>
-    //     <tbody>
-    //       <tr>
-    //         <td valign="top" width="200">
-    //           <CourseNavigation />
-    //         </td>
-    //         <td valign="top" width="100%">
-    //           {children}
-    //         </td>
-    //       </tr>
-    //     </tbody>
-    //   </table>
-    // </div>
-
-    // New Styling 2.4.0
     <div id="wd-courses">
       <h2 className="text-danger">
         <FaAlignJustify className="me-4 fs-4 mb-1" />
-        Course {cid}{" "}
-      </h2>{" "}
+        {course?.name || "Course Not Found"}
+      </h2>
       <hr />
       <div className="d-flex">
         <div className="d-none d-md-block" style={{ width: "320px" }}>
