beb000a983e8bae14167a58f4b9a8a0483006c3f
Ella <pitts.ell@northeastern.edu>
2025-10-31 13:22:09 -0400

    Add, Edit, Delete course componenet, and Add + Update course feature added

--- src/app/(Kambaz)/Dashboard/page.tsx
+++ src/app/(Kambaz)/Dashboard/page.tsx
@@ -1,32 +1,153 @@
 // This is the dashboard course page
 "use client";
+import { useState } from "react"; // add useState hook to remember informaiton bewteen renders
 import Link from "next/link";
 import * as db from "../Database";
-import { Card, CardImg, CardBody, CardTitle, CardText, Button, Row, Col } from "react-bootstrap";
+import {
+  Card,
+  CardImg,
+  CardBody,
+  CardTitle,
+  CardText,
+  Button,
+  Row,
+  Col,
+  FormControl,
+} from "react-bootstrap";
+import { v4 as uuidv4 } from "uuid";
 
 export default function Dashboard() {
-  const courses = db.courses; // This pulls from the sample database provided in Prof. Jose Annunziato's Course Repo.  
+  const [courses, setCourses] = useState<any[]>(db.courses);
+  const [course, setCourse] = useState<any>({
+    _id: "0",
+    name: "New Course",
+    number: "New Number",
+    startDate: "2023-09-10",
+    endDate: "2023-12-25",
+    image: "/images/reactjs.jpg",
+    description: "New Description",
+  });
+
+  // Adds course to dashboard initialization
+  const addNewCourse = () => {
+    // creates addnewCourse event handler that sets courses as copy of current course state array
+    const newCourse = { ...course, _id: uuidv4() }; // add course ad end fo the array, overriding _id to current time stamp
+    setCourses([...courses, newCourse]);
+  };
+
+  // Deletes course on dashboard initialization
+  const deleteCourse = (courseId: string) => {
+    // add deleteCourse event handler accepting
+    setCourses(courses.filter((course) => course._id !== courseId)); // ID of course to remove by filtering out the course by its ID
+  };
+
+  // Edit button
+const updateCourse = () => {
+    setCourses(
+      courses.map((c) => {
+        if (c._id === course._id) {
+          return course;
+        } else {
+          return c;
+        }
+      })
+    );
+  };
 
   return (
     <div id="wd-dashboard">
       <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
-      <h2 id="wd-dashboard-published">Published Courses ({courses.length})</h2> <hr />
+      {/* Add New Course Button */}
+      <h5>
+        New Course
+        <button
+          className="btn btn-primary float-end"
+          id="wd-add-new-course-click"
+          onClick={addNewCourse}
+        >
+          Add{" "}
+        </button>
+        <button
+          className="btn btn-warning float-end me-2"
+          onClick={updateCourse}
+          id="wd-update-course-click"
+        >
+          Update{" "}
+        </button>
+        {/* add a button to invoke addNewCourse */}
+      </h5>
+      <hr /> <br />
+      <FormControl
+        value={course.name}
+        className="mb-2"
+        onChange={(e) => setCourse({ ...course, name: e.target.value })}
+      />
+      <FormControl
+        value={course.description}
+        rows={3}
+        onChange={(e) => setCourse({ ...course, description: e.target.value })}
+      />
+      <h2 id="wd-dashboard-published">Published Courses ({courses.length})</h2>{" "}
+      <hr />
       <div id="wd-dashboard-courses">
         <Row xs={1} md={2} lg={3} xl={4} className="g-4">
           {courses.map((course) => (
-            <Col key={course._id} className="wd-dashboard-course" style={{ width: "300px" }}>
+            <Col
+              key={course._id}
+              className="wd-dashboard-course"
+              style={{ width: "350px" }}
+            >
               <Card>
-                <Link href={`/Courses/${course._id}/Home`}
-                    className="wd-dashboard-course-link text-decoration-none text-dark">
-                  <CardImg src="/images/introtocompsci.jpg" variant="top" height={160} />
+                <Link
+                  href={`/Courses/${course._id}/Home`}
+                  className="wd-dashboard-course-link text-decoration-none text-dark"
+                >
+                  <CardImg
+                    src="/images/introtocompsci.jpg"
+                    variant="top"
+                    height={160}
+                  />
                   <CardBody className="card-body">
                     <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">
-                     {course.name} </CardTitle>
-                     <CardText className="wd-dashboard-course-description overflow-hidden" style={{ height: "100px" }}>
-                      {course.description}</CardText>
-                      <Button variant="primary">Go</Button>
-                      </CardBody>
-                      </Link></Card>
+                      {course.name}
+                    </CardTitle>
+                    <CardText
+                      className="wd-dashboard-course-description overflow-hidden"
+                      style={{ height: "100px" }}
+                    >
+                      {course.description}
+                    </CardText>
+
+                    <div className="d-flex justify-content-between">
+                    <Button variant="primary">Go</Button>
+
+                    {/* Editing a course */}
+                    <button
+                      id="wd-edit-course-click"
+                      onClick={(event) => {
+                        event.preventDefault();
+                        setCourse(course);
+                      }}
+                      className="btn btn-warning me-2 float-end"
+                    >
+                      Edit
+                    </button>
+
+                    {/* Delete Course Button */}
+                    <button
+                      onClick={(evenet) => {
+                        evenet.preventDefault();
+                        deleteCourse(course._id);
+                      }}
+                      className="btn btn-danger float-end"
+                      id="wd-delete-course-click"
+                    >
+                      Delete
+                    </button>
+                    </div>
+                  </CardBody>
+                </Link>
+              </Card>
             </Col>
           ))}
         </Row>
