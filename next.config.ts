62e958a99f644d5fb8ca795fe76c699801895d8c
Ella <pitts.ell@northeastern.edu>
2025-10-31 11:54:46 -0400

    Bypassing some ESLint features

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Other config options here (if any)
+  eslint: {
+    // This option prevents builds from failing on lint errors
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
