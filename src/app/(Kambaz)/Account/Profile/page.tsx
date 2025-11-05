25e83243cfeef08fad12a96261d539f074fdd483
Ella <pitts.ell@northeastern.edu>
2025-11-05 14:55:54 -0500

    improved account sign in

--- src/app/(Kambaz)/Account/Profile/page.tsx
+++ src/app/(Kambaz)/Account/Profile/page.tsx
@@ -1,65 +1,21 @@
 // This is the profile page
 import Link from "next/link";
-import { FormControl, FormSelect } from "react-bootstrap";
-
 export default function Profile() {
   return (
     <div id="wd-profile-screen">
       <h3>Profile</h3>
-      <FormControl
-        id="wd-username"
-        defaultValue="alice"
-        placeholder="username"
-        className="mb-2"
-      />
-      <FormControl
-        id="wd-password"
-        defaultValue="123"
-        placeholder="password"
-        type="password"
-        className="mb-2"
-      />
-      <FormControl
-        id="wd-firstname"
-        defaultValue=""
-        placeholder="First Name"
-        className="mb-2"
-      />
-      <FormControl
-        id="wd-lastname"
-        defaultValue=""
-        placeholder="Last Name"
-        className="mb-2"
-      />
-      <FormControl
-        id="wd-dob"
-        defaultValue="2000-01-01"
-        type="date"
-        className="mb-2"
-      />
-      <FormControl
-        id="wd-email"
-        defaultValue=""
-        placeholder="email"
-        type="email"
-        className="mb-2"
-      />
-      <FormSelect
-        id="wd-role"
-        defaultValue="FACULTY"
-        className="mb-2"
-      >
-        <option value="USER">User</option>
-        <option value="ADMIN">Admin</option>
-        <option value="FACULTY">Faculty</option>
-        <option value="STUDENT">Student</option>
-      </FormSelect>
-      <Link 
-        href="/Account/Signin" 
-        className="btn btn-danger w-100"
-      >
-        Sign out
-      </Link>
+      <input defaultValue="alice" placeholder="username" className="wd-username"/><br/>
+      <input defaultValue="123"   placeholder="password" type="password"
+             className="wd-password" /><br/>
+      <input defaultValue="Alice" placeholder="First Name" id="wd-firstname" /><br/>
+      <input defaultValue="Wonderland" placeholder="Last Name" id="wd-lastname" /><br/>
+      <input defaultValue="2000-01-01" type="date" id="wd-dob" /><br/>
+      <input defaultValue="alice@wonderland" type="email" id="wd-email" /><br/>
+      <select defaultValue="FACULTY" id="wd-role">
+        <option value="USER">User</option>       <option value="ADMIN">Admin</option>
+        <option value="FACULTY">Faculty</option> <option value="STUDENT">Student</option>
+      </select><br/>
+      <Link href="Signin" > Sign out </Link>
     </div>
-  );
-}
+);}
+
--- src/app/(Kambaz)/Account/Signup/page.tsx
+++ src/app/(Kambaz)/Account/Signup/page.tsx
@@ -1,24 +1,21 @@
-// This page is for signup
-
 import Link from "next/link";
-import { FormControl } from "react-bootstrap";
-
 export default function Signup() {
   return (
     <div id="wd-signup-screen">
-      <h1>Sign up</h1>
-      <FormControl id="wd-username" placeholder="username" className="mb-2"/>
-      <FormControl id="wd-password" placeholder="password" type="password" className="mb-2" />
-      <FormControl id="wd-verify-password"
+      <h3>Sign up</h3>
+      <input placeholder="username" className="wd-username" />
+      <br />
+      <input placeholder="password" type="password" className="wd-password" />
+      <br />
+      <input
         placeholder="verify password"
         type="password"
-        className="mb-2"/>
-      <Link id="wd-signup-btn" 
-      href="/Account/Profile"
-      className="btn btn-primary w-100 mb-2"> Sign up </Link>
+        className="wd-password-verify"
+      />
       <br />
-      <Link href="/Account/Signin"
-      className="wd-signin-link"> Sign in </Link>
+      <Link href="Profile"> Sign up </Link>
+      <br />
+      <Link href="Signin"> Sign in </Link>
     </div>
   );
 }
