d3210dd332bd38ba58e1c32ae1639a81c43d2fce
Ella <pitts.ell@northeastern.edu>
2025-11-05 16:10:12 -0500

    makign vercel work

---
+++ src/app/Labs/Lab4/ReduxExamples/HelloRedux/page.tsx
@@ -0,0 +1,14 @@
+// "use client";
+// export const dynamic = 'force-dynamic'; // added due to reactjs build error
+// import { useSelector } from "react-redux"; 
+
+// export default function HelloReudx() {
+//     const { message } = useSelector((state: { helloReducer: { message: string }}) => state.helloReducer);
+//     return (
+//         <div id="wd-hello-redux">
+//             <h3>Hello Redux</h3>
+//             <h4>{message}</h4>
+//             <hr/>
+//         </div>
+//     );
+// }
