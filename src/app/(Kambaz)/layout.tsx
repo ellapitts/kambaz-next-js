fac6f494c606564511251003c4dd51a1e0459f70
Ella <pitts.ell@northeastern.edu>
2025-11-08 17:57:01 -0500

    making database store for redux state

--- src/app/(Kambaz)/layout.tsx
+++ src/app/(Kambaz)/layout.tsx
@@ -2,35 +2,25 @@
 /* Should be responsisble for the layout of the main Kambaz navigation sidebar:
 - contains NEU page, Account, Dashboard, Courses, Calendar, Inbox, Labs
 */
-
+"use client";
 import { ReactNode } from "react";
 import KambazNavigation from "./kambaz-homepage-navigation";
 import "./styles.css";
+import store from "./store";
+import { Provider } from "react-redux";
 
 export default function KambazLayout({
   children,
 }: Readonly<{ children: ReactNode }>) {
   return (
-    // Old code with the table
-    // <div id="wd-kambaz">
-    //   <table>
-    //   <tbody>
-    //       <tr>
-    //         <td valign="top" width="200"><KambazNavigation /></td>
-    //         <td valign="top" width="100%">{children}</td>
-    //       </tr>
-    //     </tbody>
-    //   </table>
-    // </div>
+    <Provider store={store}>
+     <div className="d-flex" id="wd-kambaz">
+       <div>
+         <KambazNavigation />
+       </div>
+       <div className="flex-fill ps-3 wd-main-content-offset">{children}</div>
+     </div>
+   </Provider>
 
-    // New code 2.4 Styling Kambaz with CSS and Bootstrap
-    <div id="wd-kambaz">
-      <div className="d-flex">
-        <div>
-          <KambazNavigation />
-        </div>
-        <div className="wd-main-content-offset p-3 flex-fill">{children}</div>
-      </div>
-    </div>
   );
 }
