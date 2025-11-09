8cfdf2712ee817301897dd6eb14ac8ecf5c90a1e
Ella <pitts.ell@northeastern.edu>
2025-11-09 15:24:57 -0500

    Add account reducer

--- src/app/(Kambaz)/Account/AccountNavigation.tsx
+++ src/app/(Kambaz)/Account/AccountNavigation.tsx
@@ -1,14 +1,28 @@
-// Links to sign in, sign up ,and profile pages
-// relative links, remain in account
 "use client";
-
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { Nav, NavItem, NavLink } from "react-bootstrap";
+import { useSelector } from "react-redux";
+import { RootState } from "../store";
 export default function AccountNavigation() {
+  const { currentUser } = useSelector(
+    (state: RootState) => state.accountReducer
+  );
+  const links = currentUser ? ["Profile"] : ["Signin", "Signup"];
+  const pathname = usePathname();
   return (
-    <div id="wd-account-navigation">
-      <Link href="Signin"> Signin </Link> <br />
-      <Link href="Signup"> Signup </Link> <br />
-      <Link href="Profile"> Profile </Link> <br />
-    </div>
+    <Nav variant="pills">
+      {links.map((link) => (
+        <NavItem key={link}>
+          <NavLink
+            as={Link}
+            href={link}
+            active={pathname.endsWith(link.toLowerCase())}
+          >
+            {link}{" "}
+          </NavLink>{" "}
+        </NavItem>
+      ))}
+    </Nav>
   );
 }
