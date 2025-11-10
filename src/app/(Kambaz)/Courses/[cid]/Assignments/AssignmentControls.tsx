7efd0a6e3681cf535e2baf2730bb42d3ab0aaf40
Ella <pitts.ell@northeastern.edu>
2025-11-09 20:17:44 -0500

    Add Account reducer and implement assignments feature

--- src/app/(Kambaz)/Courses/[cid]/Assignments/AssignmentControls.tsx
+++ src/app/(Kambaz)/Courses/[cid]/Assignments/AssignmentControls.tsx
@@ -1,41 +1,22 @@
-import { Button, Form, InputGroup } from "react-bootstrap";
-import { FaPlus, FaSearch } from "react-icons/fa";
+"use client";
+
+import { useParams, useRouter } from "next/navigation";
+import { Button } from "react-bootstrap";
+import { FaPlus } from "react-icons/fa";
 
 export default function AssignmentControls() {
+  const { cid } = useParams();
+  const router = useRouter();
+
   return (
-    <div id="wd-assignments-controls" className="text-nowrap">
-      <Button 
-        variant="danger" 
-        size="lg" 
-        className="me-1 float-end"
-        id="wd-add-assignment-btn"
+    <div className="d-flex justify-content-end mb-3">
+      <Button
+        variant="danger"
+        onClick={() => router.push(`/Courses/${cid}/Assignments/new`)}
       >
-        <FaPlus className="position-relative me-2" style={{ bottom: "1px" }} />
+        <FaPlus className="me-2" />
         Assignment
       </Button>
-      
-      <Button 
-        variant="secondary" 
-        size="lg" 
-        className="me-1 float-end"
-        id="wd-add-group-btn"
-      >
-        <FaPlus className="position-relative me-2" style={{ bottom: "1px" }} />
-        Group
-      </Button>
-      
-      <div className="d-inline-block" style={{ width: "300px" }}>
-        <InputGroup>
-          <span className="input-group-text bg-white border-end-0">
-            <FaSearch />
-          </span>
-          <Form.Control
-            type="text"
-            placeholder="Search for Assignment"
-            className="border-start-0"
-          />
-        </InputGroup>
-      </div>
     </div>
   );
 }
---
+++ src/app/(Kambaz)/Courses/[cid]/Assignments/reducer.ts
@@ -0,0 +1,68 @@
+// Assignments reducer file
+import { createSlice } from "@reduxjs/toolkit";
+import { assignments } from "../../../Database";
+import { v4 as uuidv4 } from "uuid";
+
+const initialState = {
+  assignments: assignments,
+};
+
+// Reducer's initial state w/ default assignments copied from database created.
+const assignmentsSlice = createSlice({
+  // create slice
+  // name the slice
+  // set initial state
+  // declare reducer functions
+
+  name: "assignments",
+  initialState,
+  reducers: {
+    // new assignment is in action.payload
+    // update assignments in state adding new assignment
+    addAssignment: (state, { payload: assignment }) => {
+      const newAssignment: any = {
+        _id: uuidv4(),
+        title: assignment.title,
+        course: assignment.course,
+        description: assignment.description,
+        points: assignment.points,
+        dueDate: assignment.dueDate,
+        availableFrom: assignment.availableFrom,
+        availableUntil: assignment.availableUntil,
+      };
+      state.assignments = [...state.assignments, newAssignment] as any;
+    },
+
+    // assignment's ID to delete is in action.payload
+    // filter out assignment to delete
+    deleteAssignment: (state, { payload: assignmentId }) => {
+      state.assignments = state.assignments.filter(
+        (a: any) => a._id !== assignmentId
+      );
+    },
+
+    // assignment to update is in action.payload
+    // replace assignment whose ID matches action.payload._id
+    updateAssignment: (state, { payload: assignment }) => {
+      state.assignments = state.assignments.map((a: any) =>
+        a._id === assignment._id ? assignment : a
+      ) as any;
+    },
+
+    // select the assignment to edit
+    editAssignment: (state, { payload: assignmentId }) => {
+      state.assignments = state.assignments.map((a: any) =>
+        a._id === assignmentId ? { ...a, editing: true } : a
+      ) as any;
+    },
+  },
+});
+
+export const {
+  addAssignment,
+  deleteAssignment,
+  updateAssignment,
+  editAssignment,
+} = assignmentsSlice.actions;
+
+export default assignmentsSlice.reducer; // export reducer
