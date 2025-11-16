960b5641b13ab4270406305184b7ce8763f4a77e
Ella <pitts.ell@northeastern.edu>
2025-11-16 14:37:43 -0500

    Implement Lab 5: Node.js server with Express routes

--- src/app/Labs/Lab5/page.tsx
+++ src/app/Labs/Lab5/page.tsx
@@ -1,4 +1,10 @@
 // Lab 5 page.tsx
+import PathParameters from "./PathParameters";
+import EnvironmentVariables from "./EnvironmentVariables";
+import QueryParmeters from "./QueryParameters";
+import WorkingWithObjects from "./WorkingWithObjects";
+import WorkingWithArrays from "./WorkingWithArrays";
+const HTTP_SERVER = process.env.NEXT_PUBLIC_HTTP_SERVER;
 
 export default function Lab5() {
   return (
@@ -6,12 +12,17 @@ export default function Lab5() {
       <h2>Lab 5</h2>
       <div className="list-group">
         <a
-          href="http://localhost:4000/lab5/welcome" // hyperlink navigates to http://localhost:4000/lab5/welcome
+          href={`${HTTP_SERVER}/lab5/welcome`} // hyperlink navigates to http://localhost:4000/lab5/welcome
           className="list-group-item"
         >
           Welcome
         </a>
       </div>
+      < EnvironmentVariables />
+      <PathParameters />
+      <QueryParmeters />
+      <WorkingWithObjects />
+      <WorkingWithArrays />
       <hr />
     </div>
   );
