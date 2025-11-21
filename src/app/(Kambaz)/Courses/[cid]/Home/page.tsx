7b86b6520ef4eddff0b873dbce6d044b4deb3d8f
Ella <pitts.ell@northeastern.edu>
2025-11-20 19:17:02 -0500

    duplicate id removed

--- src/app/(Kambaz)/Courses/[cid]/Home/page.tsx
+++ src/app/(Kambaz)/Courses/[cid]/Home/page.tsx
@@ -1,5 +1,4 @@
-// src/app/(Kambaz)/Courses/[cid]/Home/page.tsx ---
-/* This file */
+// src/app/(Kambaz)/Courses/[cid]/Home/page.tsx 
 // This creates the home screen combining the course status and modules together.
 // Combines the Course Status with the Modules to create the Home screen for the course screen.
 import Modules from "../Modules/page";
@@ -7,23 +6,8 @@ import CourseStatus from "./coursehomepagestatus";
 
 export default function Home() {
   return (
-    // Old styling
-    // <div id="wd-home">
-    //   <table>
-    //     <tbody>
-    //       <tr>
-    //         <td valign="top" width="70%">
-    //           <Modules />
-    //         </td>
-    //         <td valign="top"><CourseStatus /></td>
-    //       </tr>
-    //     </tbody>
-    //   </table>
-    // </div>
-
-    // New
     <div id="wd-home">
-      <div className="d-flex" id="wd-home">
+      <div className="d-flex" >
         <div className="flex-fill me-3">
           <Modules />
         </div>
