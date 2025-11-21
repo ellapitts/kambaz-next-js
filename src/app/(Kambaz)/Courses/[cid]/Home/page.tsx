5e99218a63f473b4e4e2e09192d5fdce5b5c0893
Ella <pitts.ell@northeastern.edu>
2025-11-20 20:41:22 -0500

    remove duplicate id

--- src/app/(Kambaz)/Courses/[cid]/Home/page.tsx
+++ src/app/(Kambaz)/Courses/[cid]/Home/page.tsx
@@ -7,21 +7,6 @@ import CourseStatus from "./coursehomepagestatus";
 
 export default function Home() {
   return (
-    // Old styling
-    // <div id="wd-home">
-    //   <table>
-    //     <tbody>
-    //       <tr>
-    //         <td valign="top" width="70%">
-    //           <Modules />
-    //         </td>
-    //         <td valign="top"><CourseStatus /></td>
-    //       </tr>
-    //     </tbody>
-    //   </table>
-    // </div>
-
-    // New
     <div id="wd-home">
       <div className="d-flex" id="wd-home">
         <div className="flex-fill me-3">
