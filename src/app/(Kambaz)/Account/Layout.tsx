3c5dd5c955ca382b1e4f3338c6464c26dbc7f453
Ella <pitts.ell@northeastern.edu>
2025-12-05 15:32:39 -0500

    A1

--- src/app/(Kambaz)/Account/Layout.tsx
+++ src/app/(Kambaz)/Account/Layout.tsx
@@ -1,8 +1,3 @@
-// This is layout page for accounts.
-// Creates a nested layout.
-// Places all content of Sing in, Sing up and profile in a table and wraps all account pages.
-// Puts navigation on the left column and children on the right
-
 import { ReactNode } from "react";
 import AccountNavigation from "./AccountNavigation";
 export default function AccountLayout({
