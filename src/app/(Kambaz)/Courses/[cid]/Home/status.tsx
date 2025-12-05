62eaf15bd13b83e079fbeb23e8b2fcbb5859471a
Ella <pitts.ell@northeastern.edu>
2025-12-05 15:47:00 -0500

    rename status page

--- src/app/(Kambaz)/Courses/[cid]/Home/status.tsx
+++ src/app/(Kambaz)/Courses/[cid]/Home/status.tsx
@@ -1,34 +1,3 @@
-// import { MdDoNotDisturbAlt } from "react-icons/md";
-// import { FaCheckCircle } from "react-icons/fa";
-// import { BiImport } from "react-icons/bi";
-// import { LiaFileImportSolid } from "react-icons/lia";
-// import { Button } from "react-bootstrap";
-
-// export default function CourseStatus() {
-//   return (
-//     <div id="wd-course-status">
-//       <h2>Course Status</h2>
-//       <button>Collapse All</button>
-//       <button>View Progress</button>
-//       <select id="wd-publish-all">
-//         <option value="">✓ Publish All</option>
-//         <option value="publish-modules">Publish All Modules and Items</option>
-//         <option value="publish-modules-only">Publish Modules Only</option>
-//         <option value="unpublish-all">Unpublish All</option>
-//       </select>
-
-//       <button>Unpublish</button>
-//       <button>Publish</button>
-//       <button>Import Existing Content</button>
-//       <button>Choose Home Page</button>
-//       <button>View Course Stream</button>
-//       <button>New Annoucements</button>
-//       <button>View Course Notfifications</button>
-//       <button>New Analytics</button>
-//     </div>
-//   );
-// }
-
 // This is the course home page status file. 
 import { MdDoNotDisturbAlt } from "react-icons/md";
 import { FaCheckCircle } from "react-icons/fa";
