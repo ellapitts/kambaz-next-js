cb14ebae11cb5a1a067c52ba32c9e062c624563b
Ella <pitts.ell@northeastern.edu>
2025-12-05 16:26:14 -0500

    fixed CourseStatus Dashboard import

--- src/app/(Kambaz)/Dashboard/Courses/[cid]/Home/page.tsx
+++ src/app/(Kambaz)/Dashboard/Courses/[cid]/Home/page.tsx
@@ -3,34 +3,22 @@
 // This creates the home screen combining the course status and modules together.
 // Combines the Course Status with the Modules to create the Home screen for the course screen.
 import Modules from "@/app/(Kambaz)/Courses/[cid]/Modules/page";
-import CourseStatus from "@/app/(Kambaz)/Courses/[cid]/Home/coursehomepagestatus";
+import CourseStatus from "@/app/(Kambaz)/Courses/[cid]/Home/status";
 
 export default function Home() {
   return (
-    // Old styling
-    // <div id="wd-home">
-    //   <table>
-    //     <tbody>
-    //       <tr>
-    //         <td valign="top" width="70%">
-    //           <Modules />
-    //         </td>
-    //         <td valign="top"><CourseStatus /></td>
-    //       </tr>
-    //     </tbody>
-    //   </table>
-    // </div>
-
-    // New
     <div id="wd-home">
-      <div className="d-flex" id="wd-home">
-        <div className="flex-fill me-3">
-          <Modules />
-        </div>
-        <div className="d-none d-lg-block">
-          <CourseStatus />
-        </div>
-      </div>
+      <table>
+        <tbody>
+          <tr>
+            <td valign="top" width="70%">
+              <Modules />
+            </td>
+            <td valign="top"><CourseStatus /></td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
+
